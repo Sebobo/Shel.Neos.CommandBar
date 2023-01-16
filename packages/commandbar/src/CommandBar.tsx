b83916de5a48b0476e93c501b198371ba2a45d66
Sebastian Helzle <sebastian@helzle.it>
2023-01-16 21:06:51 +0100

    TASK: Move flattenCommands into its own file

--- packages/commandbar/src/CommandBar.tsx
+++ packages/commandbar/src/CommandBar.tsx
@@ -6,6 +6,7 @@ import { commandBarReducer, ACTIONS } from './state/commandBarReducer';
 import CommandBarFooter from './CommandBarFooter/CommandBarFooter';
 import CommandBarHeader from './CommandBarHeader/CommandBarHeader';
 import CommandListing from './CommandList/CommandListing';
+import { flattenCommands } from './helpers/flattenCommands';
 
 type CommandBarProps = {
     commands: HierarchicalCommandList;
@@ -23,46 +24,6 @@ const initialState: CommandBarState = {
     runningCommandId: null,
     runningCommandMessage: null,
 };
-
-/**
- * This method converts the hierarchical command list into a flat list of commands which is more convenient
- * for internal processing, whereas the hierarchical command list is more convenient for the user.
- */
-function flattenCommands(commands: HierarchicalCommandList, parentId: CommandId = null): FlatCommandList {
-    return Object.keys(commands).reduce((commandList, commandId) => {
-        const { icon, description, name, subCommands, action, canHandleQueries } = commands[commandId] as Command &
-            CommandGroup;
-
-        // Create an uniquely identifiable command id for the flat command list
-        const absoluteCommandId = parentId ? `${parentId}.${commandId}` : commandId;
-
-        // Create list of available subcommand names
-        const subCommandIds = subCommands
-            ? Object.keys(subCommands).map((subCommandId) => `${absoluteCommandId}.${subCommandId}`)
-            : [];
-
-        // Insert the processed command into the flat command list
-        commandList[absoluteCommandId] = {
-            id: absoluteCommandId,
-            name,
-            icon,
-            description,
-            action,
-            canHandleQueries,
-            subCommandIds,
-            parentId,
-        };
-
-        // Insert subcommands into the list
-        if (subCommandIds.length > 0) {
-            return {
-                ...commandList,
-                ...flattenCommands(subCommands, absoluteCommandId),
-            };
-        }
-        return commandList;
-    }, {} as FlatCommandList);
-}
 
 const CommandBar: React.FC<CommandBarProps> = ({ commands, open, toggleOpen }) => {
     const [state, dispatch] = useReducer(commandBarReducer, {
---
+++ packages/commandbar/src/helpers/flattenCommands.ts
@@ -0,0 +1,39 @@
+/**
+ * This method converts the hierarchical command list into a flat list of commands which is more convenient
+ * for internal processing, whereas the hierarchical command list is more convenient for the user.
+ */
+export function flattenCommands(commands: HierarchicalCommandList, parentId: CommandId = null): FlatCommandList {
+    return Object.keys(commands).reduce((commandList, commandId) => {
+        const { icon, description, name, subCommands, action, canHandleQueries } = commands[commandId] as Command &
+            CommandGroup;
+
+        // Create an uniquely identifiable command id for the flat command list
+        const absoluteCommandId = parentId ? `${parentId}.${commandId}` : commandId;
+
+        // Create list of available subcommand names
+        const subCommandIds = subCommands
+            ? Object.keys(subCommands).map((subCommandId) => `${absoluteCommandId}.${subCommandId}`)
+            : [];
+
+        // Insert the processed command into the flat command list
+        commandList[absoluteCommandId] = {
+            id: absoluteCommandId,
+            name,
+            icon,
+            description,
+            action,
+            canHandleQueries,
+            subCommandIds,
+            parentId,
+        };
+
+        // Insert subcommands into the list
+        if (subCommandIds.length > 0) {
+            return {
+                ...commandList,
+                ...flattenCommands(subCommands, absoluteCommandId),
+            };
+        }
+        return commandList;
+    }, {} as FlatCommandList);
+}
