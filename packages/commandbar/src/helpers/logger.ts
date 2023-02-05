7a70c54788eb7b4a509895d652e6f034eb4330db
Sebastian Helzle <sebastian@helzle.it>
2023-02-05 22:38:04 +0100

    BUGFIX: Logger colors

--- packages/commandbar/src/helpers/logger.ts
+++ packages/commandbar/src/helpers/logger.ts
@@ -1,12 +1,14 @@
+const PREFIX = '%c[COMMANDBAR]';
+
 const logger = {
-    ok: (message: string, ...args: any[]) => console.log(`%c${message}`, 'color: green', ...args),
-    error: (message: string, ...args: any[]) => console.log(`%c${message}`, 'color: red', ...args),
-    warn: (message: string, ...args: any[]) => console.warn(`%c${message}`, 'color: orange', ...args),
+    ok: (message: string, ...args: any[]) => console.log(PREFIX, 'color: green', message, ...args),
+    error: (message: string, ...args: any[]) => console.log(PREFIX, 'color: red', message, ...args),
+    warn: (message: string, ...args: any[]) => console.warn(PREFIX, 'color: orange', message, ...args),
     debug:
         // @ts-ignore
         process.env.NODE_ENV === 'production'
             ? () => null
-            : (message: string, ...args: any[]) => console.debug(`%c${message}`, 'color: blue', ...args),
+            : (message: string, ...args: any[]) => console.debug(PREFIX, 'color: lightblue', message, ...args),
 };
 
 export default logger;
