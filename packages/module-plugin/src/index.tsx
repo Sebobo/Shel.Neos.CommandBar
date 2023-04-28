298372b64e6e7d6766323996312f03ce47487b05
Sebastian Helzle <sebastian@helzle.it>
2023-04-28 14:27:07 +0200

    FEATURE: Load the plugin in all modules with Neos 8.3 and prevent duplicate registration

--- packages/module-plugin/src/index.tsx
+++ packages/module-plugin/src/index.tsx
@@ -8,10 +8,14 @@ if (process.env.NODE_ENV !== 'production') {
     require('preact/debug');
 }
 
-// Register & add the debug web component, tagName and attributes are automatically read from the component
-register(App, null, null, App.options);
+function initializeApp() {
+    // Prevent registering the custom element twice
+    if (window['SHEL_NEOS_COMMANDBAR_INITIALIZED']) return;
+    window['SHEL_NEOS_COMMANDBAR_INITIALIZED'] = true;
 
-window.addEventListener('neoscms-i18n-initialized', () => {
+    // Register & add the debug web component, tagName and attributes are automatically read from the component
+    register(App, null, null, App.options);
+
     // Get the top bar left container and create a custom element to render the command bar into
     const topBarLeft = document.querySelector('.neos-top-bar-left');
     const pluginContainer = document.createElement('command-bar-container');
@@ -24,4 +28,11 @@ window.addEventListener('neoscms-i18n-initialized', () => {
     pluginContainer.setAttribute('styleuri', commandBarStyleTag.href);
 
     topBarLeft.appendChild(pluginContainer);
-});
+}
+
+// If the api is already initialized, initialize the app as the event listener wouldn't be called anymore
+if (window.NeosCMS?.I18n?.initialized) {
+    initializeApp();
+} else {
+    window.addEventListener('neoscms-i18n-initialized', initializeApp);
+}
